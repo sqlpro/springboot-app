21ebdd253f4dc45a0d5762f0e9cbe89240fc524f
Abdul <hijaeeun@tokenamdins-MacBook-Pro.local>
2024-05-21 18:11:48 +0900

    Add Echo API

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +12,14 @@ public class HomeController {
 
     private final MemberService memberService;
 
-    @GetMapping("/index")
+    @GetMapping("/")
     public ResponseEntity<String> index() {
         memberService.regist();
         return ResponseEntity.ok("SUCCESS");
+    }
+
+    @GetMapping("echo/{message}")
+    public ResponseEntity<String> echo(@PathVariable("message") String message) {
+        return ResponseEntity.ok(message);
     }
 }
